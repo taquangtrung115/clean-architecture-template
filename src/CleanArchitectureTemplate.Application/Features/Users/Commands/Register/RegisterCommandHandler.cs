7c20537711ea6065e58270836059e3ed0818f923
trung.tq <trung.tq@TRUNGTQ-SS-HCM>
2025-03-18 09:12:18 +0700

    feat: login and register

---
+++ src/CleanArchitectureTemplate.Application/Features/Users/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitectureTemplate.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTemplate.Application.Features.Users.Commands.Register
+{
+    public class RegisterCommandHandler(UserManager<ApplicationUser> userManager
+        , SignInManager<ApplicationUser> signInManager
+        ) : IRequestHandler<RegisterCommand, IdentityResult>
+    {
+        public async Task<IdentityResult> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = request.Username,
+                Email = request.Email
+            };
+
+            var result = await userManager.CreateAsync(user, request.Password);
+
+            return result;
+        }
+    }
+}
